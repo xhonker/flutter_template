b687f71a27c798d853b0849a24dccfc583f94666
xhonker <wu_xhonker@hotmail.com>
2021-03-26 17:53:32 +0800

    feat: load local image

--- lib/utils/index.dart
+++ lib/utils/index.dart
@@ -1,3 +1,4 @@
 export 'adapt.dart';
 export 'device.dart';
 export 'shared_preferences.dart';
+export 'load_local_image.dart';
---
+++ lib/utils/load_local_image.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+Image loadLocalImage(String name, {String ext = '.png', Color color, double width, double height}) {
+  return Image.asset(
+    'lib/assets/image/$name$ext',
+    width: width ?? double.infinity,
+    height: height ?? double.infinity,
+    excludeFromSemantics: true,
+    gaplessPlayback: true,
+    color: color,
+  );
+}
